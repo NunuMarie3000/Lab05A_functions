c5538e3149cd90931e7ee5d277735de561c3f4f2
Storm OBryant <vmarie1997@gmail.com>
2022-06-10 13:17:48 -0500

    completed sum, don't know where it went before

--- js/app.js
+++ js/app.js
@@ -11,13 +11,13 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   let sumArray = [];
   let added = a + b;
-  let addedString = `The sum of ${a} and ${b} is ${added}.`;
-  sumArray.push(added, addedString);
+  let sumstring = `The sum of ${a} and ${b} is ${added}.`;
+  sumArray.push(added, sumstring);
   return sumArray;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,9 +30,6 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
-
-}
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
